ff3737ea604cfbe796afb419bfafab25c41e691b
sunsfa <1941191401@qq.com>
2020-05-15 00:13:17 +0800

    提交firstclass

---
+++ git-test/src/com/suns/git/FirstClass.java
@@ -0,0 +1,10 @@
+package com.suns.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        System.out.println("hello tom");
+        System.out.println("hello tom");
+        System.out.println("hello tom");
+    }
+}
